d8f15a2cde1baae4545c2e06185a27abd4fd2a11
Venan42 <viniciusandradebc@gmail.com>
2026-09-13 20:50:33 -0300

    feat: adiciona core providers para gerenciamento de estado e configuração do Dio

---
+++ frontend/lib/core/providers/core_providers.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final secureStorageProvider = Provider<FlutterSecureStorage>((ref) {
+  return const FlutterSecureStorage();
+});
+
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'http://127.0.0.1:8000',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+  );
+
+  return dio;
+});
